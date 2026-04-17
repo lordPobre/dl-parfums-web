eabb2a4d83559b094cfcce7ede82eb67ecc98da6
Carlos Lopez <clopez.ti@vcchile.cl>
2026-04-17 10:27:42 -0400

    arreglo contacto

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+import os
+from django.core.mail import send_mail
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Perfume,Marca
 
 def home(request):
@@ -29,7 +32,31 @@ def perfume_detalle(request, perfume_id):
     return render(request, 'detalle.html', context)
 
 def contacto(request):
-    return render(request, 'contacto.html')
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        email_cliente = request.POST.get('email')
+        mensaje = request.POST.get('mensaje')
+
+        asunto = f'Nuevo mensaje de contacto en DL Parfums de: {nombre}'
+        cuerpo = f'Nombre: {nombre}\nCorreo de contacto: {email_cliente}\n\nMensaje:\n{mensaje}'
+        mi_correo = os.environ.get('EMAIL_HOST_USER')
+
+        try:
+            send_mail(
+                asunto,
+                cuerpo,
+                mi_correo, 
+                [mi_correo],
+                fail_silently=False,
+            )
+
+            messages.success(request, 'Tu mensaje ha sido enviado. Te contactaremos pronto.')
+            return redirect('contacto') 
+            
+        except Exception as e:
+            messages.error(request, 'Hubo un problema al enviar el mensaje. Intenta nuevamente.')
+            
+    return render(request, 'tu_template_de_contacto.html')
 
 def politicas_envio(request):
     return render(request, 'politicas.html')
